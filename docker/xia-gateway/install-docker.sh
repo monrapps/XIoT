b758e29d9b89929c6e1a4ca95e646e67acacc695
MonRá <lixeiradomonra@gmail.com>
2017-04-17 15:03:20 -0700

    Closer than ever

--- docker/xia-gateway/install-docker.sh
+++ docker/xia-gateway/install-docker.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-git clone https://github.com/contiki-os/contiki.git \
-cd contiki \
-git submodule update --init \
-export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
+apt-get -y install ant openjdk-8-jdk
+
+git clone https://github.com/contiki-os/contiki.git 
+cd contiki 
+git submodule update --init 
 
