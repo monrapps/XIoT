cbc4194ea0b1efa34bc9016b7e01cf6dbb90e0a8
MonRá <lixeiradomonra@gmail.com>
2017-04-19 04:07:37 -0700

    trying to make xia-base beauty again

--- docker/xia-base/build-xia.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /xia-core
-./configure
-make
-
--- docker/xia-base/install-xia.sh
+++ docker/xia-base/install-xia.sh
@@ -4,21 +4,21 @@
 #apt-mark hold grub
 
 # make sure os is up to date
-apt-get update
-apt-get -y upgrade
+#apt-get update
+#apt-get -y upgrade
 
 # Install required packages
-apt-get install -y git g++ make openssl libssl-dev
-apt-get install -y libprotobuf-dev protobuf-compiler
-apt-get install -y python-tk python-dev python-setuptools python-crypto python-requests
-apt-get install -y swig
-apt-get install -y python-argparse
-apt-get install -y python-pygame
-apt-get install -y libffi-dev python-crypto python-protobuf python-networkx python-pip
+#apt-get install -y git g++ make openssl libssl-dev
+#apt-get install -y libprotobuf-dev protobuf-compiler
+#apt-get install -y python-tk python-dev python-setuptools python-crypto python-requests
+#apt-get install -y swig
+#apt-get install -y python-argparse
+#apt-get install -y python-pygame
+#apt-get install -y libffi-dev python-crypto python-protobuf python-networkx python-pip
 
 # tools
 #apt-get install -y vim tmux ack-grep curl socat netcat wget stunnel
-apt-get install -y net-tools
+#apt-get install -y net-tools
 
 #pip install --upgrade pip
 #pip install pynacl
