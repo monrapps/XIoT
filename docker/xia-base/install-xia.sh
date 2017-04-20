ccfe9ef039933f047ee86b07b02a1081452e077a
MonRá <lixeiradomonra@gmail.com>
2017-04-20 03:05:02 -0700

    changed branch to xia-v2

--- docker/xia-base/install-xia.sh
+++ docker/xia-base/install-xia.sh
@@ -28,7 +28,7 @@ cd /
 git clone https://github.com/XIA-Project/xia-core.git
 chmod -R 777 xia-core
 cd xia-core
-git checkout develop
+git checkout xia-v2
 
 # Delete xsockconf.ini files that are not for GENI XIA-prototype
 #find . -name "xsockconf*.ini" -exec rm -rf {} \;
