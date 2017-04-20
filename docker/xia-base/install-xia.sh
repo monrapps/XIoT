c848c6b05acfc4d5abebbbf7e4cf00929d89c876
MonRá <lixeiradomonra@gmail.com>
2017-04-19 19:44:22 -0700

    Revert "xia-base ready"
    This reverts commit 8672fbc7095abccaa53132101f4dea57fd30656c.

--- docker/xia-base/install-xia.sh
+++ docker/xia-base/install-xia.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+# keep grub UI from coming up
+#apt-mark hold grub
+
+# make sure os is up to date
+#apt-get update
+#apt-get -y upgrade
+
+# Install required packages
+#apt-get install -y git g++ make openssl libssl-dev
+#apt-get install -y libprotobuf-dev protobuf-compiler
+#apt-get install -y python-tk python-dev python-setuptools python-crypto python-requests
+#apt-get install -y swig
+#apt-get install -y python-argparse
+#apt-get install -y python-pygame
+#apt-get install -y libffi-dev python-crypto python-protobuf python-networkx python-pip
+
+# tools
+#apt-get install -y vim tmux ack-grep curl socat netcat wget stunnel
+#apt-get install -y net-tools
+
+#pip install --upgrade pip
+#pip install pynacl
+
 # get XIA source & build
 cd /
 git clone https://github.com/XIA-Project/xia-core.git
@@ -9,8 +32,6 @@ git checkout develop
 
 # Delete xsockconf.ini files that are not for GENI XIA-prototype
 #find . -name "xsockconf*.ini" -exec rm -rf {} \;
-
-cd /xia-core
 
 ./configure
 make
