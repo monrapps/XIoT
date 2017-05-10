b5f981d40b5d4a2ecce36737614cf297f463cd02
MonRá <lixeiradomonra@gmail.com>
2017-05-10 14:45:32 -0300

    For Added

--- scripts/time.sh
+++ scripts/time.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
 
+while getopts C:CR:G:GR:M:RQ:T:S option
+do
+    case "${option}"
+    in
+        C) CLIENTS=${OPTARG};;
+        CR) CLIENTROUTERS=${OPTARG};;
+        G) GATEWAYS=${OPTARG};;
+        GR) GATEWAYROUTERS=${OPTARG};;
+        M) MOTES=${OPTARG};;
+    esac
+done
+
 docker run -d xia-server
 
 sleep 5
 
-docker run -d xia-gateway
+for i in `seq 1 $GATEWAYS`;
+do
+    docker run -d xia-gateway &
+done
 
 sleep 5
 
-docker run xia-client
+for i in `seq 1 $CLIENTS`;
+do
+    docker run xia-client &
+done
+
+
 
