aba76b54dc5bb2d272b41163010dbd8d824e7265
MonRá <lixeiradomonra@gmail.com>
2017-08-01 08:45:35 -0700

    preparing auto-star topology

--- docker/xia-gateway/getlocaldag.c
+++ docker/xia-gateway/getlocaldag.c
@@ -5,7 +5,7 @@ int main(){
 	char dag[200], fid[100];
 	int sock = Xsocket(AF_XIA, SOCK_STREAM, 0);	
 	XreadLocalHostAddr(sock, dag, 200, fid, 100);	
-	FILE *fp = fopen("./dag","w");
+	FILE *fp = fopen("./localdag","w");
 	fputs(dag, fp);
         fclose(fp);
 	return 0;
---
+++ src/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys
+import os
+import time
+
+import string
+import random
+
+MOTES = 10
+GATEWAYS = 10
+GATEWAYROUTERS = 10
+
+print str(random.randint(1,MOTES))+"."+str(random.randint(1,GATEWAYS))+"."+str(random.randint(1,GATEWAYROUTERS))
+
